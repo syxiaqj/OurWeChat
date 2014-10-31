da4c04c01f6598eed63a4f341734f930d4362ae4
员员 <syxiaqj@gmail.com>
2014-10-31 17:59:44 +0800

    Imitate WeChat
    This is a demo imitating wechat and coding by swift

---
+++ OurWeChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OurWeChat
+//
+//  Created by xqj on 14/10/31.
+//  Copyright (c) 2014年 renhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
